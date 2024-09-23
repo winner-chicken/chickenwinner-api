d5b4021a413e43a49c8786fb78b2abc24825d8a5
horuscamachoavila <horus.avila@wallia.io>
2024-09-22 19:20:45 -0600

    [FEATURE] Agrega configuración de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Chicken Winner API')
+    .setDescription('The Chicken Winner API description')
+    .setVersion('0.1')
+    .addTag('chicken-winner')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.use(passport.initialize());
   app.useGlobalPipes(
     new ValidationPipe({
